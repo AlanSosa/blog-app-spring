2906205035569fe6fd511fd79b3f3d57a9e64ef3
jose carabez <jay_ge_2ce@hotmail.com>
2017-05-09 08:31:10 -0500

    Trabajando en el index

--- src/main/java/com/app/blog/controllers/HomeController.java
+++ src/main/java/com/app/blog/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package com.app.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by bacon_lover on 07/05/17.
@@ -10,8 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping(name = "/", method = GET)
+    public String index(Model model){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        model.addAttribute("list", list);
         return "index";
     }
 
