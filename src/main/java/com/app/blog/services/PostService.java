d74d6f92a4e71cbdb378e9d4ebf77c7e68dba952
Alan Sosa Mejia <bacon_lover@MacBook-Pro-de-Alan.local>
2017-05-09 17:24:48 -0500

    Blog funcionando con STUB
    Para conocer que diablos hacen las ånotations y las interfaces

--- src/main/java/com/app/blog/services/PostService.java
+++ src/main/java/com/app/blog/services/PostService.java
@@ -1,15 +1,18 @@
 package com.app.blog.services;
 
 import com.app.blog.models.Post;
+
 import java.util.List;
+
 /**
- * Created by bacon_lover on 07/05/17.
+ * Created by bacon_lover on 09/05/17.
  */
 public interface PostService {
-    public List<Post> findAll();
-    public List<Post> findLatest5();
-    public Post findById(Long id);
-    public Post create(Post post);
-    public Post edit(Post post);
-    public void deleteById(Long id);
+
+    List<Post> findAll();
+    List<Post> findLatest5();
+    Post findById(Long id);
+    Post create(Post post);
+    Post edit(Post post);
+    void deleteById(Long id);
 }
